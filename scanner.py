a7f1bd833a45e48e530ad6c3dddd5cf5e2ad0363
Sai1693 <sainaveenvanipenta@gmail.com>
2026-07-31 15:32:46 +0530

    Update scanner.py
    Added project banner

--- scanner.py
+++ scanner.py
@@ -1,1 +1,11 @@
-
+def show_banner():
+    banner = r"""
+====================================================
+          PYTHON NETWORK PORT SCANNER
+====================================================
+        Developed by Sai Naveen
+----------------------------------------------------
+ Scan open TCP ports on any target host quickly
+====================================================
+"""
+    print(banner)
